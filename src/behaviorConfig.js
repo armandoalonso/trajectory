9abf32be14ba9316b638007d591e880dc1fb3bd8
armando.alonso <armandoalonso1@gmail.com>
2023-08-25 07:52:27 -0400

    bump version

--- src/behaviorConfig.js
+++ src/behaviorConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "behavior",
   id: "piranha305_trajectory",
   name: "Trajectory",
-  version: "1.0.0.0",
+  version: "1.0.0.1",
   category:
     // "attributes",
      "movements",
